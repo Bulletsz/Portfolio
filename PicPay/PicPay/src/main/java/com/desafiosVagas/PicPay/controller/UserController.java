8604bc10d0048f9af9c81ffa1b096c6efc1705d6
Nicolas <nicolas.antunes28@gmail.com>
2026-01-31 17:43:41 -0300

    Initial commit

---
+++ PicPay/PicPay/src/main/java/com/desafiosVagas/PicPay/controller/UserController.java
@@ -0,0 +1,82 @@
+package com.desafiosVagas.PicPay.controller;
+
+import com.desafiosVagas.PicPay.model.User;
+import jakarta.transaction.Transactional;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.desafiosVagas.PicPay.repository.UserRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping ("users")
+public class UserController {
+
+    private UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity<User> cadastrar(@RequestBody User user) {
+        User saved = userRepository.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(saved);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<User> obterPorId(@PathVariable("id") UUID id) {
+
+        /* Optional<Produto> produtoSalvo = produtoRepository.findById(id);
+        return produtoSalvo.isPresent() ? produtoSalvo.get() : null; */
+
+        return userRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarPorId(@PathVariable("id") UUID id) {
+
+        if (!userRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        userRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<User> editar(
+            @PathVariable UUID id,
+            @RequestBody User user) {
+
+        return userRepository.findById(id)
+                .map(existingUser -> {
+
+                    if (user.getName() != null) {
+                        existingUser.setName(user.getName());
+                    }
+
+                    if (user.getGmail() != null) {
+                        existingUser.setGmail(user.getGmail());
+                    }
+
+                    if (user.getCpf() != null) {
+                        existingUser.setCpf(user.getCpf());
+                    }
+
+                    if (user.getPassword() != null) {
+                        existingUser.setPassword(user.getPassword());
+                    }
+
+                    User saved = userRepository.save(existingUser);
+                    return ResponseEntity.ok(saved);
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
+}
